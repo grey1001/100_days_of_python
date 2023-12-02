e002917d0dcc53390f69e06ccaa5b5e174c55780
Grey <thegrey@rocketmail.com>
2023-12-02 10:24:31 +0100

    Updated day 1 code

--- day1-10/band-name-generator.py
+++ day1-10/band-name-generator.py
@@ -1,4 +1,13 @@
-print("Welcome to band name generator!")
-city_name = input("What city did you grow up in\n")
-pet_name = input("What is the name of your pet?\n")
-print("The name of your band could be", city_name + " " + pet_name)
+# print("Welcome to band name generator!")
+# city_name = input("What city did you grow up in\n")
+# pet_name = input("What is the name of your pet?\n")
+# print("The name of your band could be", city_name + " " + pet_name)
+
+a = "40"
+b = "50"
+c = a
+a = b
+b = c
+print(a , b)
+
+# showing how to switch input values 
