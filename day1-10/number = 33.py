932af87f783a54fcedea001e75e1c3c073470a42
Grey <thegrey@rocketmail.com>
2023-12-03 09:42:13 +0100

    Update to the code

--- day1-10/number = 33.py
+++ day1-10/number = 33.py
@@ -1,3 +1,15 @@
-number = 33.6
-rounded_number = round(33.6 ,4)
-print(rounded_number)
+# number = 33.6
+# rounded_number = round(33.6 , 4.22)
+# print(rounded_number)
+
+# 1st input: enter height in meters e.g: 1.65
+height = input()
+# 2nd input: enter weight in kilograms e.g: 72
+weight = input()
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+
+new_height = float(height ** 2)
+new_weight = int(weight)
+print(type(new_height))
